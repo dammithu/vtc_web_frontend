38d9ae5deef1b800591524beb0da85eb72a25013
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2026-02-23 18:16:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 1002,  // <-- set your desired port here
+  },
 })
