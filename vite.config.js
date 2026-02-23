87926b789e801b06c0b617981d1500f54bff58e3
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2026-02-23 23:10:45 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5175, // <-- choose >1024, e.g., 10002
-    host: true,  // optional, exposes server on your EC2 public IP
+    port: 6000,
   },
 })
